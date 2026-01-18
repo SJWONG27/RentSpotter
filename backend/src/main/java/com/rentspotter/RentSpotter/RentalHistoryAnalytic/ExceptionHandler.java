4e5239fecc559b8d4f60d918199cc10815ab59da
Master <ss@>
2026-01-18 14:45:41 +0800

    history_v2

--- backend/src/main/java/com/rentspotter/RentSpotter/RentalHistoryAnalytic/ExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package com.rentspotter.RentSpotter.RentalHistoryAnalytic;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class ExceptionHandler {
-}
