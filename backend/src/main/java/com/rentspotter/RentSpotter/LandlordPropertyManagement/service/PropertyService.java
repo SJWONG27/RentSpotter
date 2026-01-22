492e21659a67f84be678b800f0f44969a13ad360
xiaoxuan <hengxiaoxuan2003@gmail.com>
2026-01-22 14:53:45 +0800

    Add missing import

--- backend/src/main/java/com/rentspotter/RentSpotter/LandlordPropertyManagement/service/PropertyService.java
+++ backend/src/main/java/com/rentspotter/RentSpotter/LandlordPropertyManagement/service/PropertyService.java
@@ -8,6 +8,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
